7e99cfaa0507067cccbe14038aa3316a9de91334
guillaumebarranco <guillaume.barranco8@hotmail.fr>
2016-02-04 18:05:22 +0100

    first commit

---
+++ classes/Article.class.php
@@ -0,0 +1,17 @@
+<?php
+
+class Article {
+	protected $id;
+	protected $title;
+	protected $content;
+	protected $date;
+	protected $category;
+	protected $picture;
+	protected $id_auteur;
+	protected $intro;
+
+	public function __construct() {
+		
+	}
+
+}
---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE HTML>
+
+<html>
+	<head>
+		<title>Good Planet</title>
+		<meta charset="utf-8" />
+		<link rel="stylesheet" href="css/index.css">
+	</head>
+
+	<body>
+		<div>Hello</div>
+
+		<script src="js/main.js"></script>
+	</body>
+</html>
